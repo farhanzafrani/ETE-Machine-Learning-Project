bac7cbdfd3a84dea746b25e195dc04e5f36db923
Muhammad Farhan <mfarhanzafran@gmail.com>
2023-09-14 00:56:10 +0500

    Logger and exception files has been updated

---
+++ src/exception.py
@@ -0,0 +1,21 @@
+import sys
+
+def error_message_details(error,error_detail:sys):
+    _,_,error_tb = error_detail.exc_info()
+
+    file_name = error_tb.tb_frame.f_code.co_filename
+    line_no = error_tb.tb_lineno
+
+    error_message = "Error has occured in the script of [{1}] in the line number [{2}] and the error is [{3}]".format(
+        file_name, line_no, str(error))
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self,error,error_detail:sys):
+        super(CustomException,self).__init__()
+        self.error_message = error_message_details(error,error_detail)
+
+    def __str__(self):
+        return self.error_message
+
+
