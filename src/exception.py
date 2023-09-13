4ac9ce1acd9711dd5752efb331d8e8ca4ab24d51
Muhammad Farhan <mfarhanzafran@gmail.com>
2023-09-14 01:32:56 +0500

    Logger and Exception file has updated

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,6 @@
 import sys
+import logging
+
 
 def error_message_details(error,error_detail:sys):
     _,_,error_tb = error_detail.exc_info()
@@ -6,7 +8,7 @@ def error_message_details(error,error_detail:sys):
     file_name = error_tb.tb_frame.f_code.co_filename
     line_no = error_tb.tb_lineno
 
-    error_message = "Error has occured in the script of [{1}] in the line number [{2}] and the error is [{3}]".format(
+    error_message ="Error has occured in the script of [{0}] in the line number [{1}] and the error is [{2}]".format(
         file_name, line_no, str(error))
     return error_message
 
--- src/logger.py
+++ src/logger.py
@@ -3,8 +3,8 @@ import os
 from datetime import datetime
 
 log_file = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-log_path = os.path.join(os.getcwd,'logs',log_file)
-os.mkdir(log_path,exist_ok=True)
+log_path = os.path.join(os.getcwd(),'logs',log_file)
+os.makedirs(log_path,exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(log_path,log_file)
 
@@ -14,3 +14,4 @@ logging.basicConfig(
     format = "[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level= logging.INFO
 )
+
